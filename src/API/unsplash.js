c8bd5c487072af9427c1dde193f7dc7ae3ae6125
yuuto <yuuttoo@gmail.com>
2019-10-03 15:12:53 +0800

    抓圖片後調整高度並排版

--- src/API/unsplash.js
+++ src/API/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID enter api here'
+        Authorization: 'Client-ID Enter API here'
     }
 })
